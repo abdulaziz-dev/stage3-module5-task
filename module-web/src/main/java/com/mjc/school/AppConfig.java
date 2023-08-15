a4d2b66d58703fa8400e111587252769520d5ed0
abdulaziz-dev <axmedovabdulaziz2001@gmail.com>
2023-08-15 19:10:45 +0500

    war config

--- module-web/src/main/java/com/mjc/school/AppConfig.java
+++ module-web/src/main/java/com/mjc/school/AppConfig.java
@@ -2,6 +2,8 @@ package com.mjc.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,11 +17,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
         "com.mjc.school.service",
         "com.mjc.school.controller",
         "com.mjc.school"})
-public class Main {
+public class AppConfig extends SpringBootServletInitializer{
 
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+            return builder.sources(AppConfig.class);
+        }
 
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
+        public static void main(String[] args) {
+            SpringApplication.run(AppConfig.class, args);
+        }
 
 }
